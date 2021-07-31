4042f437497f95848cf8d634e0f08285ed7cedb4
Chandra Nandan <chandranandan.chandrakar@gmail.com>
2021-07-31 16:21:56 +0530

    pub/sub

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 import http from "http";
 import ws from "websocket"
 import redis from "redis";
+
+
+const Server = ws.server
+
+
+const subscriber = redis.createClient({
+  port      : 6379,              
+  host      : 'rds'} );
+
+const publisher = redis.createClient({
+  port      : 6379,              
+  host      : 'rds'} );
+
+
